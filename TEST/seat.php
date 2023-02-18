30daa337df18f91abc91e65506ff8822383b4b4c
chanboreychea <cheaborey25@gmail.com>
2023-02-18 14:43:01 +0700

    seat update js

--- TEST/seat.php
+++ TEST/seat.php
@@ -1,13 +1,17 @@
 <style>
+    tr{
+        display: flex;
+        gap: 10px;
+    }
     td {
+        cursor: pointer;
+        background-color: #444451;
+        height: 26px;
+        width: 32px;
+        margin: 3px;
+        font-size: 50px;
         border-top-left-radius: 10px;
         border-top-right-radius: 10px;
-        width: 30px;
-        height: 30px;
-        cursor: pointer;
-        background-color: gray;
-        margin-bottom: 20px;
-        border: 1px solid black;
     }
 
     .box.active {
@@ -19,8 +23,8 @@
     }
 </style>
 <h3>Count: <span id="count"></span></h3>
-<h5>Seat: <span id="seat"></span></h5>
-<p>Rows: <span id="row"></span><span>-</span><span id="cell"></span></p>
+<h4>Seat: <span id="seat"></span></h4>
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -35,7 +39,7 @@ if ($conn->connect_error) {
 }
 
 
-$sql = "SELECT seat_id ,rows_number, seat_number FROM seats where hall_id=4";
+$sql = "SELECT seat_id ,rows_number, seat_number FROM seats where hall_id=1";
 $result = $conn->query($sql);
 
 
@@ -52,7 +56,7 @@ $result = $conn->query($sql);
                     for ($j = 0; $j < $row['seat_number']; $j++) { ?>
 
                         <td>
-                            <!-- <div class="box"></div> -->
+                            
                         </td>
 
                     <?php
@@ -70,38 +74,45 @@ $result = $conn->query($sql);
 
 
     let seats = [];
-    let count = 0;
+    let count=0;
 
     for (var i = 0; i < table.rows.length; i++) {
 
         for (var j = 0; j < table.rows[i].cells.length; j++) {
 
             table.rows[i].cells[j].onclick = function() {
+                
+                rindex = this.parentElement.rowIndex;
+                cindex = this.cellIndex;
+                let r = rindex + 1;
+                let c = cindex + 1;
+                if(r==1){
+                    r="A"
+                }
+                else if(r==2){
+                    r="B"
+                }
+                else if(r==2){
+                    r="C"
+                }
+                else{
+                    r="D"
+                }
+
+
                 if (this.classList.toggle('active')) {
                     ++count;
-                    rindex = this.parentElement.rowIndex;
-                    cindex = this.cellIndex;
-                    let r = rindex + 1;
-                    let c = cindex + 1;
-                    seats.push(`${r}-${c}`)
-                    
-                    // document.getElementById('row').innerHTML = r
-                    // document.getElementById('cell').innerHTML = c
+                    seats.push(`${r} ${c}`)
+                    // let ind = seats.indexOf(`${r}-${c}`)
+                    // console.log(Number(`${c}`))covert to number
+
                 } else {
                     --count;
-                    rindex = this.parentElement.rowIndex;
-                    cindex = this.cellIndex;
-                    let r = rindex + 1;
-                    let c = cindex + 1;
-                    let inde = seats.indexOf(`${r}-${c}`)
-                    seats.splice(inde,1)
-                  
-                    // var ind = seats.indexOf(r + "-" + c)
-                    // console.log(ind)
-                    // document.getElementById('row').innerHTML = ""
-                    // document.getElementById('cell').innerHTML = ""
+                    let inde = seats.indexOf(`${r} ${c}`)
+                    seats.splice(inde, 1)
                 }
-                document.getElementById('seat').innerHTML = seats;
+
+                document.getElementById('seat').innerHTML = seats.sort().reverse();
                 document.getElementById('count').innerHTML = count;
 
             }
